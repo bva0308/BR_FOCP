b998cfa38c609eae37f6db06fc007a2489e52833
bva0308 <126636494+bva0308@users.noreply.github.com>
2023-11-29 17:48:06 +0545

    week-3

---
+++ week_3/task8.py
@@ -0,0 +1,14 @@
+names = ["Tom", "Ram", "Rahul"] 
+
+print("Bob" in names)
+print("Mark" in names)
+
+
+message = "Hello there, my name is Bibha"
+print("name" in message) 
+print("message" in message)
+
+
+colors = ["red", "green", "blue"]
+print("red" in colors)
+print("purple" in colors)
